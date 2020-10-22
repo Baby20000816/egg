fee620bd10bbf317740ab656b77f7793d23e7e96
unknown <1273163614@qq.com>
2020-10-22 13:54:08 +0800

    用户注册

--- config/plugin.js
+++ config/plugin.js
@@ -2,13 +2,16 @@
 
 /** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
-  
-cors:{
-  enable: true,
-  package: 'egg-cors',
-},
+  cors: {
+    enable: true,
+    package: 'egg-cors',
+  },
+  sequelize: {
+    enable: true,
+    package: 'egg-sequelize',
+  },
+  valparams: {
+    enable: true,
+    package: 'egg-valparams',
+  },
 };
